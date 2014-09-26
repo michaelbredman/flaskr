68b9cde0d8623bc3c801c2f4473c6fc273b758dc
michaelbredman <mredman@gmail.com>
2014-09-26 14:40:31 -0700

    more travis configuration V

--- test_login.py
+++ test_login.py
@@ -1,11 +1,7 @@
-
-# from selenium.webdriver.firefox.webdriver import WebDriver
-# from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time, unittest, sys, copy
+import unittest
+import sys
+import copy
 import wd.parallel
 import os
 
@@ -13,8 +9,6 @@ import os
 class TestLogin(unittest.TestCase):
 
     def setUp(self):
-        # self.wd = WebDriver()
-        # self.wd.implicitly_wait(60)
 
         desired_capabilities = []
 
@@ -49,26 +43,13 @@ class TestLogin(unittest.TestCase):
         browser['version'] = '7'
         desired_capabilities += [browser]
 
-
         self.drivers = wd.parallel.Remote(
             desired_capabilities=desired_capabilities,
             command_executor="http://" + os.environ['SAUCE_USERNAME'] + ":" + os.environ['SAUCE_ACCESS_KEY'] + "@ondemand.saucelabs.com:80/wd/hub"
         )
 
-    #@saucehelper.timeit
     @wd.parallel.multiply
     def test_parallel(self):
-        print "Setting up tests...."
-
-        # print "Native Events Supported: " + str(self.driver.desired_capabilities)
-
-        print "Working on : " + self.driver.desired_capabilities['platform'] \
-        + " " + self.driver.desired_capabilities['browserName'] \
-        + " " + self.driver.desired_capabilities['version']
-        print "Working on session : " + self.driver.session_id
-
-        #saucehelper.report_test_name(self.driver.session_id, self.driver.session_id, os.environ['SAUCE_USERNAME'], os.environ['SAUCE_ACCESS_KEY'])
-        #saucehelper.report_build_number("Sauce", self.driver.session_id, os.environ['SAUCE_USERNAME'], os.environ['SAUCE_ACCESS_KEY'])
 
         self.driver.get("http://localhost:5000")
         self.driver.find_element_by_link_text("log in").click()
@@ -82,15 +63,6 @@ class TestLogin(unittest.TestCase):
         self.driver.find_element_by_link_text("log out").click()
 
 
-        # wait = (self.driver, 30)
-        # print "Wait" + str(wait)
-        # condition = EC.text_to_be_present_in_element((By.TAG_NAME, "html"), "Private Banking")
-        # print "********CONDIITON***********" + str(condition)
-        # This is currently saving the image locally how do I do it on Sauce?
-        self.driver.save_screenshot(self.driver.desired_capabilities['platform'] \
-        + "-" + self.driver.desired_capabilities['version'] + "-" + self.driver.session_id + ".png")
-        # wait.until(condition)
-
     @wd.parallel.multiply
     def tearDown(self):
         print "Tearing down test...."
@@ -101,7 +73,6 @@ class TestLogin(unittest.TestCase):
         else:
             print "Test failed.... https://saucelabs.com/tests/" + self.driver.session_id
 
-        #saucehelper.report_status(status, self.driver.session_id, os.environ['SAUCE_USERNAME'], os.environ['SAUCE_ACCESS_KEY'])
         self.driver.quit()
 
 
